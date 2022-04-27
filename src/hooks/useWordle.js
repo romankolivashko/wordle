e786e78114396a49106cea11454da807fbe684a5
Roman Kolivashko <rkolivashko@gmail.com>
2022-04-26 19:13:19 -0700

    Added keyup logic

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -22,8 +22,23 @@ const useWordle = (solution) => {
 
   // handle keyup event & track current guess
   // if user presses enter, add the new guess
-  const handleKeyup = () => {
+  const handleKeyup = ({key}) => {
+    
+    if(key === 'Backspace') {
+      setCurrentGuess((prev)=>{
+        return prev.slice(0, -1)
+      })
+      return
+    }
 
+    if(/^[A-Za-z]$/.test(key)){
+      if(currentGuess.length < 5){
+        setCurrentGuess((prev)=>{
+          return prev + key
+        })
+      }
+
+    }
   }
 
   return {turn, currentGuess, guesses, isCorrect, handleKeyup}
